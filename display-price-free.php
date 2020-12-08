4834a11ff0b54860ea383bd5871307918448a20a
Renzo Tejada <renzzo09@gmail.com>
2020-12-07 21:06:45 -0500

    feat_ agregando  domain para traducción

--- display-price-free.php
+++ display-price-free.php
@@ -4,8 +4,10 @@
   Plugin Name: Display Price Free
   Description:  Display FREE if Price Zero or Empty - WooCommerce Single Product
   Version:     0.0.1
-  Author:            Renzo Tejada
-  Author URI:        https://renzotejada.com/
+ * License:           GNU General Public License v3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       rt-price-free
+ * Domain Path:       /language
  */
 
 add_filter( 'woocommerce_get_price_html', 'rt_display_price_free_zero_empty', 9999, 2 );
