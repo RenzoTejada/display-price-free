3ce6fb7f7b512571736af8098aae753520fd1f3f
Renzo Tejada <renzzo09@gmail.com>
2020-12-07 21:08:22 -0500

    feat: agregando traslate al texto

--- display-price-free.php
+++ display-price-free.php
@@ -14,7 +14,7 @@ add_filter( 'woocommerce_get_price_html', 'rt_display_price_free_zero_empty', 99
    
 function rt_display_price_free_zero_empty( $price, $product ){
     if ( '' === $product->get_price() || 0 == $product->get_price() ) {
-        $price = '<span class="woocommerce-Price-amount amount">Gratis</span>';
+        $price = '<span class="woocommerce-Price-amount amount">'.__('FREE', 'rt-price-free').'</span>';
     }  
     return $price;
 }
